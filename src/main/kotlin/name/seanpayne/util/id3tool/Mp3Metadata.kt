6129fecbe83c688c0a545f291c8ed1f22b4b55bb
Sean Payne <seantpayne@gmail.com>
2018-11-24 13:19:05 -0800

    Simplified conversion using ID3Wrapper

--- src/main/kotlin/name/seanpayne/util/id3tool/Mp3Metadata.kt
+++ src/main/kotlin/name/seanpayne/util/id3tool/Mp3Metadata.kt
@@ -1,5 +1,6 @@
 package name.seanpayne.util.id3tool
 
+import com.mpatric.mp3agic.ID3Wrapper
 import com.mpatric.mp3agic.Mp3File
 
 data class Mp3FileMetadata(
@@ -21,14 +22,15 @@ data class Id3Metadata(
         val genre:Int?) {
     companion object {
         fun convertId3(mp3File: Mp3File) : Id3Metadata? {
+            val wrapper = ID3Wrapper(mp3File.id3v1Tag, mp3File.id3v2Tag)
             return Id3Metadata(
-                    album = if (mp3File.hasId3v2Tag()) mp3File.id3v2Tag.album else mp3File.id3v1Tag?.album,
-                    artist = if (mp3File.hasId3v2Tag()) mp3File.id3v2Tag.artist else mp3File.id3v1Tag?.artist,
-                    title = if (mp3File.hasId3v2Tag()) mp3File.id3v2Tag.title else mp3File.id3v1Tag?.title,
-                    track = if (mp3File.hasId3v2Tag()) mp3File.id3v2Tag.track else mp3File.id3v1Tag?.track,
-                    year = if (mp3File.hasId3v2Tag()) mp3File.id3v2Tag.year else mp3File.id3v1Tag?.year,
-                    comment = if (mp3File.hasId3v2Tag()) mp3File.id3v2Tag.comment else mp3File.id3v1Tag?.comment,
-                    genre = if (mp3File.hasId3v2Tag()) if(mp3File.id3v2Tag.genre == -1) null else mp3File.id3v2Tag.genre else if(mp3File.id3v1Tag.genre == -1) null else mp3File.id3v1Tag?.genre
+                    album = wrapper.album,
+                    artist = wrapper.artist,
+                    title = wrapper.title,
+                    track = wrapper.track,
+                    year = wrapper.year,
+                    comment = wrapper.comment,
+                    genre = wrapper.genre
             )
         }
     }
