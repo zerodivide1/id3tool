f876d2af07742fe39685cefa3c8e8d82e0dfff2d
Sean Payne <seantpayne@gmail.com>
2018-11-24 13:09:54 -0800

    Replaced Moshi with GSON

--- src/main/kotlin/name/seanpayne/util/id3tool/Main.kt
+++ src/main/kotlin/name/seanpayne/util/id3tool/Main.kt
@@ -3,9 +3,8 @@ package name.seanpayne.util.id3tool
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.google.gson.Gson
 import com.mpatric.mp3agic.Mp3File
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class Id3Tool : CliktCommand() {
     override fun run() = Unit
@@ -19,11 +18,7 @@ class Id3ToJson: CliktCommand(help = "Output ID3 to JSON") {
 
         val metadata = mp3File.jsonMetadata()
 
-        val moshi = Moshi.Builder()
-                .add(KotlinJsonAdapterFactory())
-                .build()
-        val jsonAdapter = moshi.adapter(Mp3FileMetadata::class.java)
-        val jsonResult = jsonAdapter.toJson(metadata)
+        val jsonResult = Gson().toJson(metadata)
 
         println(jsonResult)
     }
